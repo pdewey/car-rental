58b72f4ad8610b40f6ae2501bcb176051ef6ad9c
Taylor <taylorskalyo@gmail.com>
2013-04-18 22:30:32 -0400

    Create main class

---
+++ CarRental/src/cs4400/ViewPanel.java
@@ -0,0 +1,11 @@
+package cs4400;
+import java.awt.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class ViewPanel extends JPanel {
+	public ViewPanel(){	
+		setBorder(BorderFactory.createLineBorder(getBackground(), 15));
+		setPreferredSize(new Dimension(800,600));
+	}
+}
