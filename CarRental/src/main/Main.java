2baabc54fa02015a669d75626f5c8cb44a5ef033
Paul Dewey <pdewey17@gmail.com>
2013-04-20 11:22:28 -0400

    Restructured the code a bit

---
+++ CarRental/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+import panels.ViewPanel;
+
+
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("GT Car Rental System");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		ViewPanel panel = new ViewPanel();
+		
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
